230c6a31954df5ac1cab8b69af8320eb330e4081
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-21 14:36:02 +0100

    checkpoint(data): update project files

--- src/analytics.py
+++ src/analytics.py
@@ -2,5 +2,9 @@ from src.db import run_sql
 
 def monthly_revenue():
     return run_sql("""
-    SELECT DATA_TRUNC('')
+    SELECT DATA_TRUNC('month', 'created_at') AS month,
+        SUM('total') AS revenue
+    FROM orders
+    GROUP BY 1
+    ORDER BY 1
     """)
